8d2839f7b45a210491e53c612bb4be7739b1c9e0
sasakazu <sasakura.company@gmail.com>
2018-09-05 20:14:15 +0900

    first commit

---
+++ iamMattyo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iamMattyo
+//
+//  Created by 笹倉 一也 on 2018/09/05.
+//  Copyright © 2018年 sasakura.company. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
